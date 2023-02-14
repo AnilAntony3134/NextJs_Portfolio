fc0722a9dde2db91ba5ad81f96a4318e7fb88332
Anil Antony <anilantony3134@gmail.com>
2023-02-14 22:12:14 +0530

    fix: added

---
+++ components/Skills/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaReact } from 'react-icons/fa';
+import { SiRedux, SiJavascript, SiNetlify } from 'react-icons/si';
+import { IoLogoFirebase } from 'react-icons/io5';
+import { DiJqueryLogo, DiCss3 } from 'react-icons/di';
+import { AiFillHtml5 } from 'react-icons/ai';
+
+const Skills = () => {
+    return (
+        <>
+            <span className='mr-6'><FaReact /></span>
+            <span className='mr-6'><SiRedux /></span>
+            <span className='mr-6'><SiJavascript /></span>
+            <span className='mr-6'><IoLogoFirebase /></span>
+            <span className='mr-6'><DiJqueryLogo /></span>
+            <span className='mr-6'><AiFillHtml5 /></span>
+            <span className='mr-6'><FaReact /></span>
+            <span className='mr-6'><FaReact /></span>
+            <span className='mr-6'><FaReact /></span>
+            <span className='mr-6'><FaReact /></span>
+            <span className='mr-6'><FaReact /></span>
+        </>
+
+    )
+}
+
+export default Skills
+
+
+
